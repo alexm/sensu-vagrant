7d6d2cde45c936960991749379866cedb0583dfb
Alex Muntada <alexm@alexm.org>
2013-10-11 18:11:27 +0200

    use puppetlabs/apt module

--- modules/sensu/manifests/init.pp
+++ modules/sensu/manifests/init.pp
@@ -1,16 +1,12 @@
 class sensu {
-  exec { 'import-sensu-apt-key':
-    command => '/usr/bin/wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | /usr/bin/sudo apt-key add -',
-    unless  => '/usr/bin/sudo apt-key list | grep -i sensu';
-  }
+  include apt
 
-  exec { 'apt-get update':
-    command => '/usr/bin/sudo apt-get update';
-  }
-
-  file { '/etc/apt/sources.list.d/sensu.list':
-    ensure  => present,
-    content => 'deb http://repos.sensuapp.org/apt sensu main',
-    require => Exec['import-sensu-apt-key'];
+  apt::source { 'sensu':
+    location    => 'http://repos.sensuapp.org/apt',
+    release     => 'sensu',
+    repos       => 'main',
+    include_src => false,
+    key         => '7580C77F',
+    key_source  => 'http://repos.sensuapp.org/apt/pubkey.gpg';
   }
 }
